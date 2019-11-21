c272c58f979a79a6b628a6afa366926c7439e387
Rafał Major <rmaj@dgs.com>
2019-11-21 08:43:32 +0100

    add Get method to PrimeFactors class

--- PrimeFactorsKataAcademy/PrimeFactors.cs
+++ PrimeFactorsKataAcademy/PrimeFactors.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+
 namespace PrimeFactorsKataAcademy
 {
     public class PrimeFactors
     {
+        public static IEnumerable<int> Get(int i)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
