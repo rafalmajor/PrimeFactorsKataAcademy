19eea27bf58b3dccc79fa026ba983db76919489f
Rafał Major <rmaj@dgs.com>
2019-11-21 08:52:17 +0100

    assert 6

--- PrimeFactorsKataAcademy/UnitTest1.cs
+++ PrimeFactorsKataAcademy/UnitTest1.cs
@@ -14,6 +14,7 @@ namespace PrimeFactorsKataAcademy
             Assert.Equal(new List<int> { 3 }, PrimeFactors.Get(3));
             Assert.Equal(new List<int> { 2, 2 }, PrimeFactors.Get(4));
             Assert.Equal(new List<int> { 5 }, PrimeFactors.Get(5));
+            Assert.Equal(new List<int> { 2, 3 }, PrimeFactors.Get(6));
         }
     }
 }
